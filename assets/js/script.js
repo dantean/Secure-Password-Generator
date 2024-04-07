df9cb40db7f152a610267b0601cd5cc133db78b6
Joseph Monticelli <jdmonticelli@gmail.com>
2024-04-07 00:04:07 -0400

    typed up all starter code

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,53 @@ var setIntervalId
 var timeRemaining=questionData.length * 15
 var index=0
 
+
+
+function startQuiz(){
+    introEl.classList.add("hide")
+    questionSelectionEl.classList.remove("hide")
+    renderQuestion()
+    setIntervalId=setInterval(startTimer , 1000 )
+}
+function renderQuestion(){
+    messageEl.innterHTML=""
+    questionTitleEl.textContent=questionData[index].title
+    choiceListEl.textcontent=""
+
+    for(var i=0; i<questionData[index].choices.length; i++ ){
+        var li=document.createElement("li")
+        var button=document.createElement("button")
+        button.textContent=questionData[index].choices[i]
+        li.appendChild(button)
+        choiceListEl.appendChild(li)
+    }
+}
+function startTimer(){
+    timerEl.textContent=timeRemaining--
+}
+function nextQuestion(event){
+    var currentChoiceBtn=event.target
+    var solution=questionData[index].solution
+    index++
+    if(index < questionData.length){
+        questionData
+        if(currentChoiceBtn.textContent === solution){
+            messageEl.innterHTML="<h4>Correct!</h4>"
+            timeRemaining=timeRemaining-10
+        }
+    
+    selfTimeout (renderQuestion,500)
+
+}else{
+    endQuiz()
+}
+}
+
+function endQuiz(){
+    clearInterval(setIntervalId)
+    questionSelectionEl.classList.add("hide")
+    initialInputEl.classList.remove("hide")
+    scoreEl.textContent=timerEl.textContent
+}
+startQuizEl.addEventListener("click",startQuiz)
+choiceListEl.addEventListener("click",nextQuestion)
