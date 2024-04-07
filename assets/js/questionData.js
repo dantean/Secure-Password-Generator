1128d3a5ea2a5d1915a6656e4ee7103427a7bd12
Joseph Monticelli <jdmonticelli@gmail.com>
2024-04-06 23:50:59 -0400

    saved progress on js code

---
+++ assets/js/questionData.js
@@ -0,0 +1,27 @@
+var questionData = [
+    {
+        title:"Q1",
+        choices:["C1", "C2", "C3", "C4"],
+        solution:"C3"
+    },
+    {
+        title:"Q2",
+        choices:["C1","C2","C3","C4",],
+        solution:"C1"
+    },
+    {
+        title:"Q3",
+        choices:["C1","C2","C3","C4"],
+        solution:"C4"
+    },
+    {
+        title:"Q4",
+        choices:["C1","C2","C3","C4"],
+        solution:"C2"
+    },
+    {
+        title:"Q5",
+        choices:["C1","C2","C3","C4"],
+        solution:"C1"
+    }
+]
---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var introEl=document.getElementById("intro")
+var questionSelectionEl=document.getElementById("question-section")
+var initialInputEl=document.getElementById("initial-input")
+var highscoreEl=document.getElementById("highscore")
+var startQuizEl=document.getElementById("start-quiz")
+var questionTitleEl=document.getElementById("choice-list")
+var timerEl=document.getElementById("timer")
+var messageEl=document.getElementById("message")
+var scoreEl=document.getElementById("score")
+var setIntervalId
+var timeRemaining=questionData.length * 15
+var index=0
+
